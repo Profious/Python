8f824d44f790b933a39c720e0b1e632fd3c29272
Neema <neemaahm@gmail.com>
2016-08-11 21:52:21 -0400

    still in the process of fixing stuff, most current version as of 8/4/16

--- Battleship.py
+++ Battleship.py
@@ -2,7 +2,7 @@
 Battleship style game.  It first asks you to pick a spot for you battleship, then asks you to guess
 the enemy's location.  The enemy also shoots at the player each turn, and code is put into place as
 to insure that neither the player nor the enemy shoot/place their battleships in a stop that doesn't
-make sense
+make sense :D
 """
 # Todo: integrate the pebcak system (pebcak_sys(pebcak_numb))
 # Todo: fix the whole system at the bottom
